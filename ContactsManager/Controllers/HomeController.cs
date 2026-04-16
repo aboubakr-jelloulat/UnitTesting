829e835f6de6bb1aa48226eb93fde01faa066da6
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-04-16 16:58:59 +0100

    Add project files.

---
+++ ContactsManager/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ContactsManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
