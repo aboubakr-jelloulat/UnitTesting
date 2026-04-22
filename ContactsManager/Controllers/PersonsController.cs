bdcce723058ba06a3939ff6cf3847f020895f404
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-04-22 11:35:52 +0100

    Implement Search and Sort in View

--- ContactsManager/Controllers/PersonsController.cs
+++ ContactsManager/Controllers/PersonsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ServiceContacts;
 using ServiceContacts.DTOs;
+using ServiceContacts.Enums;
 
 namespace ContactsManager.Controllers;
 
@@ -19,18 +20,26 @@ public class PersonsController : Controller
     [Route("person/index")]
     [Route("/")]
     [HttpGet]
-    public IActionResult Index()
+    public IActionResult Index(string searchBy, string? searchString, string sortedBy = nameof(PersonResponseDTO.PersonName),
+        SortedOrderOptions sortedOrder = SortedOrderOptions.ASC)
     {
         ViewBag.SearchFields = new Dictionary<string, string>()
         {
             {nameof(PersonResponseDTO.PersonName),  "Person Name"},
             {nameof(PersonResponseDTO.Email),       "Email"},
-            {nameof(PersonResponseDTO.CountryId),   "Country"},
             {nameof(PersonResponseDTO.Adress), "Address"},
         };
 
+        ViewBag.CurrentSearchBy     = searchBy;
+        ViewBag.CurrentSearchString = searchString;
 
-        var persons = _personService.GetAllPersons();
+        ViewBag.CurrentSortedBy = sortedBy;
+        ViewBag.CurrentSortedOrder = sortedOrder;
+
+        var persons = _personService.GetFiltredPersons(searchBy, searchString);
+
+        persons = _personService.GetSortedPersons(persons, sortedBy, sortedOrder);
+
 
         var result = persons.Select(p => new PersonViewModel
         {
