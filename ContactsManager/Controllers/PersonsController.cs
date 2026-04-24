c2864821cfba731059ecca9f93259b6fc1f26a1e
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-04-24 13:50:21 +0100

    Add Update Logic

--- ContactsManager/Controllers/PersonsController.cs
+++ ContactsManager/Controllers/PersonsController.cs
@@ -5,6 +5,9 @@ using ServiceContacts.Enums;
 
 namespace ContactsManager.Controllers;
 
+[Route("person")]
+// or
+//[Route("[controller]")]
 public class PersonsController : Controller
 {
     private readonly IPersonService     _personService;
@@ -16,7 +19,11 @@ public class PersonsController : Controller
     }
 
 
-    [Route("person/index")]
+    [Route("index")]
+    //or
+    //[Route("[action]")]
+    //or
+    //[Route("[controller]/[action]")]
     [Route("/")]
     [HttpGet]
     public IActionResult Index(string searchBy, string? searchString, string sortedBy = nameof(PersonResponseDTO.PersonName),
@@ -44,4 +51,115 @@ public class PersonsController : Controller
 
         return View(persons);
     }
+
+
+
+    [Route("create")]
+    [HttpGet]
+    public IActionResult Create()
+    {
+        // Load countries and genders for dropdown options
+        ViewBag.Countries = _countriesService.GetAllCountries();
+        ViewBag.Genders = Enum.GetValues(typeof(GenderOptions)).Cast<GenderOptions>().ToList();
+
+        return View();
+    }
+
+    [Route("create")]
+    //or
+    //[Route("[action]")]
+    [HttpPost]
+    public IActionResult Create(PersonAddRequestDTO model)
+    {
+       
+        if (!ModelState.IsValid)
+        {
+            // Load countries and genders for dropdown options
+            ViewBag.Countries = _countriesService.GetAllCountries();
+            ViewBag.Genders = Enum.GetValues(typeof(GenderOptions)).Cast<GenderOptions>().ToList();
+
+            return View(model);
+        }
+
+        try
+        {
+            var personResponse = _personService.AddPerson(model);
+            return RedirectToAction("Index");
+        }
+        catch (ArgumentException ex)
+        {
+            // Add general form error (shows at top)
+            ModelState.AddModelError(string.Empty, ex.Message);
+
+            // Reload dropdowns
+            ViewBag.Countries = _countriesService.GetAllCountries();
+            ViewBag.Genders = Enum.GetValues(typeof(GenderOptions)).Cast<GenderOptions>().ToList();
+
+            return View(model);
+        }
+    }
+
+
+
+    [Route("[action]")]
+    [HttpGet]
+    public IActionResult Update(Guid id)
+    {
+        var person = _personService.GetPersonById(id);
+
+        if (person is null)
+        {
+            return NotFound();
+        }
+
+        var updateModel = new PersonUpdateRequestDTO
+        {
+            Id = person.Id,
+            PersonName = person.PersonName,
+            Email = person.Email,
+            DateOfBirth = person.DateOfBirth,
+            Gender = Enum.TryParse<GenderOptions>(person.Gender, out var gender)? gender : null,
+            CountryId = person.CountryId,
+            Address = person.Adress,
+            ReceiveNewsLetters = person.ReceiveNewsLetters
+        };
+
+        // Load dropdowns
+        ViewBag.Countries = _countriesService.GetAllCountries();
+        ViewBag.Genders = Enum.GetValues(typeof(GenderOptions)).Cast<GenderOptions>().ToList();
+
+        return View(updateModel);
+    }
+
+    [Route("[action]")]
+    [HttpPost]
+    public IActionResult Update(PersonUpdateRequestDTO? model)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Countries = _countriesService.GetAllCountries();
+            ViewBag.Genders = Enum.GetValues(typeof(GenderOptions)).Cast<GenderOptions>().ToList();
+
+            return View(model);
+        }
+
+        try
+        {
+            _personService.UpdatePerson(model);
+            return RedirectToAction("Index");
+        }
+        catch (ArgumentException ex)
+        {
+            ModelState.AddModelError(string.Empty, ex.Message);
+
+            ViewBag.Countries = _countriesService.GetAllCountries();
+            ViewBag.Genders = Enum.GetValues(typeof(GenderOptions)).Cast<GenderOptions>().ToList();
+
+            return View(model);
+        }
+    }
+
+
+
+
 }
