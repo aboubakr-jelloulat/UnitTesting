18b1a200b9acd0791deaeabefa2d9ed1f0368e33
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-04-27 09:33:41 +0100

    Add validation/data annotation attributes

--- Entities/Country.cs
+++ Entities/Country.cs
@@ -1,10 +1,14 @@
-﻿namespace Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities;
 
 
 public class Country
 {
+    [Key]
     public Guid Id { get; set; }
 
+    [StringLength(100)]
     public string? CountryName { get; set; }
 
 }
--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities;
 
 public class Person
 {
+    [Key]
     public Guid Id { get; set; }
 
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
     public string PersonName { get; set; } = String.Empty;
-     
+
+    [EmailAddress]
+    [StringLength(255)]
     public string? Email { get; set; }
      
     public DateTime? DateOfBirth { get; set; }
      
     public string? Gender { get; set; }
-     
+
+    [StringLength(100)]
     public string? Adress { get; set; }
      
     public Guid? CountryId { get; set; }
